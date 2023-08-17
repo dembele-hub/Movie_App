066cf18e1aa99e614d81a59464c947961038fdd1
dembele_hub <chrisdembele5@gmail.com>
2023-08-17 14:38:46 +0100

    initial commit

---
+++ lib/Ui/model/movie.dart
@@ -0,0 +1,27 @@
+class Movies {
+
+  String title;
+  String year;
+  String rated;
+  String released;
+  String runtime;
+  String genre;
+  String director;
+  String writer;
+  String actors;
+  String plot;
+  String language;
+  String country;
+  String awards;
+  String poster;
+  String metascore;
+  String imdbRating;
+  String type;
+  String response;
+  List <String> images;
+
+  Movies(this.title, this.year, this.rated, this.released, this.runtime,
+      this.genre, this.director, this.writer, this.actors, this.plot,
+      this.language, this.country, this.awards, this.poster, this.metascore,
+      this.imdbRating, this.type, this.response, this.images);
+}
