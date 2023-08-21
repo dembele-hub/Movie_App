b550c346394c2601cb11166aef0890f149e98627
dembele_hub <chrisdembele5@gmail.com>
2023-08-21 12:46:26 +0100

    initial commit

--- lib/Ui/home.dart
+++ lib/Ui/home.dart
@@ -1,37 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'model/movie.dart';
+
 class MovieListView extends StatelessWidget {
   MovieListView({super.key});
-  //final List<Movie> movieList = Movie.getMovies();
 
-  final List movies = [
-    "Spider-Man: Across the Spider-Verse (2023)",
-    "John Wick 4 (2023)",
-    "Heart of Stone (2023)",
-    "Avengers: Endgame (2018)",
-    "Avatar: The Way of Water (2023)",
-    "Hidden Strike (2023)",
-    "Extraction 2 (2023)",
-    "The Mother (2023)",
-    "Viking Wolf (2022)",
-    "Th Grey Man (2022)",
-    "Interceptor (2022)",
-    "Carter (2017)",
-    "Spider Head (2022)",
-    "Titanic (1997)",
-    "The Avengers (2018)",
-    "Game of Thrones (2020)",
-    "Everything Everywhere All at Once (2022)",
-    "300 (2005)",
-    "Love and Fire (2020)",
-    "Blood Sisters (2022)",
-    "Heart of Gold (2022)",
-    "Top Gun: Maverick (2022)",
-    "The Batman (2016)",
-    "Jurassic World (2015)",
-    "The Lion King (2019)",
-    "Joker (2019)",
-  ];
+  final List<Movie> movieList = Movie.getMovies();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +19,7 @@ class MovieListView extends StatelessWidget {
       ),
       backgroundColor: Colors.redAccent.shade100,
       body: ListView.builder(
-          itemCount: movies.length,
+          itemCount: movieList.length,
           itemBuilder: (BuildContext context, int index) {
             return Card(
               elevation: 12,
@@ -51,26 +27,31 @@ class MovieListView extends StatelessWidget {
               child: ListTile(
                 leading: CircleAvatar(
                   child: Container(
+                    width: 200,
+                      height: 200,
                     decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(movieList[index].images[2]),
+                        fit: BoxFit.cover
+                      ),
                         borderRadius: BorderRadius.circular(13.5),
                         color: Colors.redAccent),
-                    child: const Text("M"),
+
                   ),
                 ),
-                trailing: const Text("220mb"),
+                trailing: Text(movieList[index].type),
                 title: Text(
-                  movies[index],
+                  movieList[index].title,
                   style: TextStyle(color: Colors.red.shade700),
                 ),
-                subtitle: const Text(
-                  "Good Movie",
-                  style: TextStyle(color: Colors.black),
+                subtitle: Text(movieList[index].year,
+                  style: const TextStyle(color: Colors.black),
                 ),
                 onTap: () {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const MovieListViewDetails(movieName: "Hello",)));
+                          builder: (context) => MovieListViewDetails(movieName: movieList.elementAt(index).title,)));
                 },
                 //onTap: () => debugPrint("Movie Name: ${movies.elementAt(index)}"),
               ),
@@ -78,10 +59,30 @@ class MovieListView extends StatelessWidget {
           }),
     );
   }
+  Widget movieCard(Movie movie, BuildContext context) {
+    return InkWell(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 120.0,
+        child: const Card(
+          color: Colors.black45,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text("data")
+          ],
+        )
+      ),
+      ),
+    );
+  }
+
 }
 
 class MovieListViewDetails extends StatelessWidget {
   final String movieName;
+
   const MovieListViewDetails({Key? key, required this.movieName}) : super(key: key);
 
   @override
@@ -95,13 +96,13 @@ class MovieListViewDetails extends StatelessWidget {
       body: Center(
         child: Container(
           child: ElevatedButton(
-            child: const Text("Go Back"),
             style: const ButtonStyle(
               backgroundColor: MaterialStatePropertyAll(Colors.redAccent),
             ),
             onPressed: () {
               Navigator.pop(context);
             },
+            child: const Text("Go Back"),
           ),
         ),
       ),
